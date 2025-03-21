cd114368b02768fdb962ec31a7689f5311f83058
Sara Isabel Ortiz <siortizh@eafit.edu.co>
2025-03-21 14:11:57 -0500

    Tests

--- tests/test_basic_best_fit.py
+++ tests/test_basic_best_fit.py
@@ -2,7 +2,7 @@
 import unittest
 from cont_mem_algos import best_fit
 
-class TestBasicFirstFit(unittest.TestCase):
+class TestBasicBestFit(unittest.TestCase):
 
     def test_pass_empty_map(self):
         work_memory = []
@@ -19,13 +19,72 @@ class TestBasicFirstFit(unittest.TestCase):
         self.assertEqual(search, None)
 
     def test_req_highest_list(self):       
-        work_memory = [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000C0000)]
+        work_memory =  [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000C0000)]
         req = 0x000D0000
         index = 0
         search = best_fit(work_memory, req, index)
         self.assertEqual(search, None)
 
+    def test_req_choose_last_index(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000D0000)]
+        req = 0x000D0000
+        index = 0
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(search[3], 0)
 
+    def test_req_choose_last_len(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000D0000)]
+        req = 0x000D0000
+        index = 0
+        len_work_memory = len(work_memory)
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(len(search[0]), len_work_memory - 1)
+
+    def test_req_choose_middle_index(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x000B0000, 0x000D0000), (0x00C00000, 0x000C0000)]
+        req = 0x000D0000
+        index = len(work_memory) - 1
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(search[3], 1)
+
+    def test_req_choose_middle_len(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x000B0000, 0x000D0000), (0x00C00000, 0x000C0000)]
+        req = 0x000D0000 
+        index = len(work_memory) - 1
+        len_work_memory = len(work_memory)
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(len(search[0]), len_work_memory - 1)
+
+    def test_req_choose_last_index_keep(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000E0000)]
+        req = 0x000D0000
+        index = 0
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(search[3], 2)
+
+    def test_req_choose_last_len_keep(self):
+        work_memory = [(0x00A00000, 0x000C0000), (0x00B00000, 0x000C0000), (0x00C00000, 0x000E0000)]
+        req = 0x000D0000
+        index = 0
+        len_work_memory = len(work_memory)
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(len(search[0]), len_work_memory)
+
+    def test_req_choose_best_fit_len(self):
+        work_memory = [(0x00A00000, 0x000D0A0), (0x00B00000, 0x000D000A), (0x00C00000, 0x000D0000)]
+        req = 0x000D0000
+        index = 0
+        len_work_memory = len(work_memory)
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(len(search[0]), len_work_memory - 1)
+
+    def test_req_choose_best_fit_index(self):
+        work_memory = [(0x00A00000, 0x000D0A0), (0x00B00000, 0x000D000A), (0x00C00000, 0x000D0000)]
+        req = 0x000D0000
+        index = 0
+        search = best_fit(work_memory, req, index)
+        self.assertEqual(search[3], 0)
+        
 if __name__ == '__main__':
     unittest.main()
 
